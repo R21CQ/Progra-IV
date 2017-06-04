f2871e9d19751c2a3f397cfd246cc728d37580a5
R21CQ <rebechevez@gmail.com>
2017-06-03 19:05:00 -0500

    Update producto.php

--- view/producto/producto.php
+++ view/producto/producto.php
@@ -30,7 +30,7 @@
                 <tr>
                     <td><input type=radio name=Producto value=<?php echo $r->idProducto; ?> ></td>
                     <td><?php echo $r->idProducto; ?></td>
-                    <td><img src="data:image/jpg; base64,<?php echo base64_encode($r->imagen); ?>"/></td>
+                    <td><img width="40" height="40" src="data:image/jpg; base64,<?php echo base64_encode($r->imagen); ?>"/></td>
                     <td><?php echo $r->tipoProducto == 1 ? 'Bicicletas' : 'Implementos'; ?></td>
                     <td><?php echo $r->descripcion; ?></td>
                     <td><?php echo $r->precio; ?></td>
