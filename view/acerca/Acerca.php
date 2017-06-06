35da5d55b24c7a8dd48e24ba215d7ecca956978a
Giancarlo <Giancarlo@HP>
2017-06-05 23:18:26 -0600

    Vista Acerca, se incluyo una nueva imagen para la vista

--- view/acerca/Acerca.php
+++ view/acerca/Acerca.php
@@ -1,12 +1,42 @@
-<div class="row">
-    <div class="col-md-6">
+<!-- CSS -->
+<link rel="stylesheet" href="archivos/css/estiloAcerca.css" type="text/css"/>
 
-    </div>
-    <div class="col-md-6">
 
+
+<div class="col-md-12">
+    <div class="row text-center">
+        <div class="col-md-6" id="idMision">
+            <div>
+                <img src="archivos/imagenes/mision.gif" width="200" height="150" class="img-responsive">
+            </div>
+            <div>
+                <h3>Dar un mejor servicio a los todos los usuarios que necesitan de nuestros servicios, atendiendo sus requerimientos y ofreciendole buenos productos para lograr así una excelente satisfacción de nuestros clientes.</h3>
+            </div>
+        </div>
+
+        <div class="col-md-6" id="idVision">
+            <div>
+                <img src="archivos/imagenes/vision.gif" width="200" height="150" class="img-responsive">
+            </div>
+            <div>
+                <h3>Llegar a consolidarnos como una empresa en el comercio de bicicletas y repuestos de estas, destacando la seriedad, servicio y atención que se brindan en este ciclo, además liderando la innovación de productos y repuestos que nos permitiran ser más competitivos</h3>
+            </div>
+        </div>
     </div>
 </div>
 
-<div class="col-md-12">
 
+<div class="col-md-12" id="descripcion">    
+    <div class="col-md-4" class="center-block">
+        <img src="archivos/imagenes/nosotros.png" width="200" height="150" class="img-responsive">
+    </div>
+        
+    <div class="col-md-8">
+        <h1>Descripción</h1>
+        
+        <h3>Somos una empresa que brinda servicios en venta de bicletas de la línea básica, 
+            media y profesional, tambien contamos con repuestos y variedad de accesorios 
+            deportivos de Ciclismo, realizamos revisiones y reparaciones de Bicicletas.
+        </h3>
+    </div>
 </div>
