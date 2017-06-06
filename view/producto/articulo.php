c3c30014c2db1bba3a3fd0105308767537a267a4
x-MGCA-x <telosabez@gmail.com>
2017-06-06 01:51:59 -0600

    libreria minicart para el button comprar articulo buton paga

--- view/producto/articulo.php
+++ view/producto/articulo.php
@@ -1,33 +1,60 @@
 <link rel="stylesheet" href="archivos/css/estiloCajas.css" type="text/css"/>
+<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
 <h1 class="page-header">Articulos</h1>
-<form action="?c=Articulo" method="post">
 
 
 
-    <?php foreach ($this->modelArticulo->Listar() as $r): 
+    <?php foreach ($this->modelArticulo->Listar() as $r):
         ?>
-    
-    <div id="cajaArticulos">
 
 
+        <div class="col-xs-6 col-sm-3 col-md-3">
+            
+            
+            <div class="thumbnail">
+                
+                <div class="caption">
 
-            <?php $valor = $r->idProducto; ?>
+                    <?php $valor = $r->idProducto; ?>
 
-            <img id="tamanoImagen" src="data:image/jpg; base64,<?php echo base64_encode($r->imagen); ?>"/>
-            <br>
-            <label name="Producto:"> Producto: <?php echo $r->idProducto; ?></label>
-            <br>
-            <label name="Clase:"> Clase: <?php echo $r->tipoProducto == 1 ? 'Bicicletas' : 'Implementos'; ?></label>
-            <br>
-            <label name="Descripcion:">Descripcion: <?php echo $r->descripcion; ?></label>
-            <br>
-            <label name="Precio:"> Precio: <?php echo $r->precio; ?></label>
+                    <img id="tamanoImagen" src="data:image/jpg; base64,<?php echo base64_encode($r->imagen); ?>"/>
+                    
+                    <label name="descripcion">Descripcion: <?php echo $r->descripcion; ?></label>
+                    <br>
+                    <label name="idProducto"> Producto: <?php echo $r->idProducto; ?></label>
+                    <br>
+                    <label name="clase"> Clase: <?php echo $r->tipoProducto == 1 ? 'Bicicletas' : 'Implementos'; ?></label>
+                    <br>
+                    <label name="precio"> Precio: <?php echo $r->precio; ?></label>
 
-            <button class="btn btn-info btn-lg btn-block">Agregar</button>
+                    <a href="#" class="btn btn-default producto" precio="<?php echo $r->precio; ?>" titulo="<?php echo $r->descripcion; ?>" role="button">Comprar</a></p>
+                </div>
+</div>
+        </div>
+            
+        <?php endforeach; ?> 
 
+<script src="archivos/js/minicart.js"></script>
+<script>
+    // configuración inicial del carrito 
+    paypal.minicart.render({
+        strings: {
+            button: 'Pagar'
+            , buttonAlt: "Total"
+            , subtotal: 'Total:'
+            , empty: 'No hay productos en el carrito'
+        }
+    });
+    // Eventos para agregar productos al carrito
 
-        </div> 
-    <?php endforeach; ?> 
+    $('.producto').click(function (e) {
+        e.stopPropagation();
+        paypal.minicart.cart.add({
+            business: 'maikel.chavarriaalvarado@gmail.com', // Cuenta paypal para recibir el dinero
+            item_name: $(this).attr("titulo"),
+            amount: $(this).attr("precio"),
+            currency_code: 'USD',
+        });
+    });
 
-
-</form>
+</script>
